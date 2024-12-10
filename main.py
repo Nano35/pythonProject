cc990db2faea9c139002b497e2f575c42ac260bf
Nano35 <nikolasha24@bk.ru>
2024-12-10 18:24:46 +0300

    bot1

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import os
+import telebot
 
-print("penes")
-print("hui")
+API_TOKEN = "7272715477:AAGvjBEXoXqX9-5e_X3V5WrOTmcYTHf2ziw"
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
