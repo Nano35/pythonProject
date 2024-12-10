968c802977f4acb8aae5b6470164545e57f1c8ff
ThePirozhkov <igromansers@gmail.com>
2024-12-10 23:17:10 +0300

    Добавлена возможность нажимать кнопку "Привет" и теперь бот на входе проверяет наличие юзера в бд

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import sqlite3
 import telebot
 from telebot import types
 
-API_TOKEN = "7272715477:AAGvjBEXoXqX9-5e_X3V5WrOTmcYTHf2ziw"
-bot = telebot.TeleBot(API_TOKEN)
+import config
 
+bot = telebot.TeleBot(config.get_api_key())
+conn = sqlite3.connect('db/python.db', check_same_thread=False)
+cursor = conn.cursor()
 
 @bot.message_handler(commands=['start']) #команды
 def send_welcome(message):
@@ -12,49 +14,51 @@ def send_welcome(message):
 Привет! Я бот фонда: "Измени Одну Жизнь"
 Я здесь чтобы помочь приемным родителям собрать всю информацию о прошлом их приемного ребенка и его настоящем уже в новой семье\
 """)
-    markup = types.InlineKeyboardMarkup()
+    inline_markup = types.InlineKeyboardMarkup()
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     btn1 = types.InlineKeyboardButton(text='Наш сайт', url='https://changeonelife.ru/about/mission/')
-    markup.add(btn1)
-    bot.send_message(message.from_user.id, "По кнопке ниже можно перейти на наш сайт", reply_markup = markup)
-
-
-#@bot.message_handler(content_types=['text'])
-#def get_text_messages(message):
-#    if message.text == '👋 Поздороваться':
-#        markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #создание новых кнопок
-#        btn1 = types.KeyboardButton('Как стать автором на Хабре?')
-#        btn2 = types.KeyboardButton('Правила сайта')
-#        btn3 = types.KeyboardButton('Советы по оформлению публикации')
-#        markup.add(btn1, btn2, btn3)
-#        bot.send_message(message.from_user.id, '❓ Задайте интересующий вопрос', reply_markup=markup) #ответ бота
-
-
-
-conn = sqlite3.connect('db/python.db', check_same_thread=False)
-cursor = conn.cursor()
-
-def db_table_val(user_id: int, user_name: str, user_surname: str, username: str):
-    cursor.execute('INSERT INTO test (user_id, user_name, user_surname, username) VALUES (?, ?, ?, ?)', (user_id, user_name, user_surname, username))
-    conn.commit()
-
-
+    hi_btn = types.KeyboardButton("Привет")
+    inline_markup.add(btn1)
+    keyboard.add(hi_btn)
+    bot.send_message(message.from_user.id, "По кнопке ниже можно перейти на наш сайт", reply_markup = inline_markup)
+    bot.send_message(message.from_user.id, "Выберите действия, которые вы хотите совершить:" , reply_markup=keyboard)
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.from_user.id, 'Привет! Ваше имя добавленно в базу данных!')
-        us_id = message.from_user.id
-        us_name = message.from_user.first_name
-        us_sname = message.from_user.last_name
-        username = message.from_user.username
+   if message.text == 'Привет':
+       if check_user(message.from_user.id):
+           bot.send_message(message.from_user.id, 'Привет! Вы уже были зарегистрированы!')
+       else:
+        add_user(message.from_user.id, message.from_user.first_name, message.from_user.last_name, message.from_user.username)
+        bot.send_message(message.from_user.id, 'Привет! Ваше имя добавлено в базу данных!')
 
-        db_table_val(user_id=us_id, user_name=us_name, user_surname=us_sname, username=username)
+def check_user(user_id):
+    cursor.execute('SELECT 1 FROM test WHERE user_id = ? LIMIT 1', (user_id,))
+    result = cursor.fetchone()
+    return result
 
+def add_user(user_id: int, user_name: str, user_surname: str, username: str):
 
+    cursor.execute('')
 
-@bot.message_handler(func=lambda message: True) #Отвечает на сообщение если не видит команду
-def echo_message(message):
-    bot.reply_to(message, """
-    Не существует такой команды""")
+    cursor.execute('INSERT INTO test (user_id, user_name, user_surname, username) VALUES (?, ?, ?, ?)',
+                   (user_id, user_name, user_surname, username))
+    conn.commit()
+
+#
+#
+# @bot.message_handler(content_types=['text'])
+# def get_text_messages(message):
+#     if message.text.lower() == 'привет':
+#         bot.send_message(message.from_user.id, 'Привет! Ваше имя добавленно в базу данных!')
+#         us_id = message.from_user.id
+#         us_name = message.from_user.first_name
+#         us_sname = message.from_user.last_name
+#         username = message.from_user.username
+#
+#         db_table_val(user_id=us_id, user_name=us_name, user_surname=us_sname, username=username)
+#
+#
+#
 
 bot.infinity_polling()
