47ee410ada87f56db8a79552b80ce5ee9e3c37ee
ThePirozhkov <igromansers@gmail.com>
2024-12-11 18:18:52 +0300

    Добавил хуйни

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ bot = telebot.TeleBot(config.get_api_key())
 conn = sqlite3.connect('db/python.db', check_same_thread=False)
 cursor = conn.cursor()
 
+child_data = ""
+
+STATE_WAITING_FOR_ANSWER = 0
+ANSWER_RECEIVED = 1
+
 @bot.message_handler(commands=['start']) #команды
 def send_welcome(message):
     bot.reply_to(message, """\
@@ -31,6 +36,19 @@ def get_text_messages(message):
        else:
         add_user(message.from_user.id, message.from_user.first_name, message.from_user.last_name, message.from_user.username)
         bot.send_message(message.from_user.id, 'Привет! Ваше имя добавлено в базу данных!')
+       keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+       reg_btn = types.KeyboardButton("📕 Собрать книгу жизни")
+       keyboard.add(reg_btn)
+       bot.send_message(message.from_user.id, "Выберите действие:", reply_markup=keyboard)
+   elif message.text == '📕 Собрать книгу жизни':
+       bot.send_message(message.from_user.id, "Напишите ФИО ребенка")
+       bot.register_next_step_handler(message, add_child_data)
+
+def add_child_data(message):
+    global child_data
+    child_data = message.text
+    bot.reply_to(message, f"ФИО ребенка: {child_data}")
+
 
 def check_user(user_id):
     cursor.execute('SELECT 1 FROM test WHERE user_id = ? LIMIT 1', (user_id,))
@@ -42,21 +60,5 @@ def add_user(user_id: int, user_name: str, user_surname: str, username: str):
     cursor.execute('INSERT INTO test (user_id, user_name, user_surname, username) VALUES (?, ?, ?, ?)',
                    (user_id, user_name, user_surname, username))
     conn.commit()
-
-#
-#
-# @bot.message_handler(content_types=['text'])
-# def get_text_messages(message):
-#     if message.text.lower() == 'привет':
-#         bot.send_message(message.from_user.id, 'Привет! Ваше имя добавленно в базу данных!')
-#         us_id = message.from_user.id
-#         us_name = message.from_user.first_name
-#         us_sname = message.from_user.last_name
-#         username = message.from_user.username
-#
-#         db_table_val(user_id=us_id, user_name=us_name, user_surname=us_sname, username=username)
-#
-#
-#
 
 bot.infinity_polling()
