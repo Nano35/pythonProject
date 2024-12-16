34bc28779992c4f96447490768c095b00eb7d254
ThePirozhkov <igromansers@gmail.com>
2024-12-16 15:09:19 +0300

    я твою мать ебал 228 ха лох эщкере

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import sqlite3
+from email.policy import default
+
+import requests
 
 import telebot
 from telebot import types
@@ -12,6 +15,9 @@ cursor = conn.cursor()
 global fio
 global birthday
 global when_i_birth
+global my_present
+global my_future
+global file_path
 
 STATE_WAITING_FOR_ANSWER = 0
 ANSWER_RECEIVED = 1
@@ -48,23 +54,59 @@ def get_text_messages(message):
        bot.register_next_step_handler(message, add_child_data)
 
 def add_child_data(message):
+    global fio
     fio = message.text
     bot.reply_to(message, f"ФИО ребенка: {fio}")
-    bot.send_message(message.from_user.id, "Напишите ДР ребенка")
+    bot.send_message(message.from_user.id, "Загрузите фото ребенка")
+    bot.register_next_step_handler(message, add_child_photo)
+
+def add_child_photo(message):
+    file = bot.get_file(message.photo[-1].file_id)
+    global file_path
+    file_path = f"photo_{file.file_id}.jpg"  # Путь к сохраненному файлу который сохраняется в бд
+    file_url = f"https://api.telegram.org/file/bot{config.get_api_key()}/{file_path}" # Реальный путь (можете по нему перейти там в консоли он выводится) НЕ ИСПОЛЬЗОВАТЬ В БД ТАК КАК ПАЛИТСЯ BOT API KEY
+    print(file_url)
+    response = requests.get(file_url, stream=True)
+    response.raise_for_status()  # Проверяем на ошибки
+
+    with open(f"photo_{file.file_id}.jpg", "wb") as f:  # Сохраняем файл на сервер
+        for chunk in response.iter_content(chunk_size=8192):
+            f.write(chunk)
+    bot.send_message(message.from_user.id, "Напишите день рождения ребенка:")
     bot.register_next_step_handler(message, add_birthday)
 
 def add_birthday(message):
+    global birthday
     birthday = message.text
-    bot.reply_to(message, f"ДР ребенка: {birthday}")
-    bot.send_message(message.from_user.id, "*Мое прошлое*", parse_mode="MarkdownV2")
-    bot.send_message(message.from_user.id, "Напишите информацию о том когда он родился")
-    bot.register_next_step_handler(message, when_i_birth)
+    bot.reply_to(message, f"День рождения ребенка: {birthday}")
+    bot.send_message(message.from_user.id, """*Моё прошлое*
+Расскажите о прошлом ребенка \(о том когда он родился и где он родился\)""", parse_mode="MarkdownV2")
+    bot.send_message(message.from_user.id, "Напишите информацию о том когда он родился и где он родился", parse_mode="MarkdownV2")
+    bot.register_next_step_handler(message, add_when_i_birth)
 
 def add_when_i_birth(message):
+    global when_i_birth
     when_i_birth = message.text
+    bot.send_message(message.from_user.id, """*Моё настоящее*
+Расскажите о настоящем ребенка \(о том где он сейчас живет, люди которые с ним, распорядок дня и его свободное время\)""", parse_mode="MarkdownV2")
+    bot.register_next_step_handler(message, add_my_present)
 
+def add_my_present(message):
+    global my_present
+    my_present = message.text
+    bot.send_message(message.from_user.id, """*Моё будущее*
+Расскажите о будущем ребенка \(о том кем он хочет быть, о его будущей семье, о его будущем доме\)""", parse_mode="MarkdownV2")
+    bot.register_next_step_handler(message, add_child_db)
 
+def add_child_db(message): # Сохраняем path в базу данных
+    global my_future
+    my_future = message.text
+    resume = when_i_birth + "\n" + my_present + "\n" + my_future
 
+    cursor.execute("INSERT INTO child (fio, birthday, resume, photo) VALUES (?, ?, ?, ?)",
+                   (fio, birthday, resume, file_path))  # user_id - ID пользователя из Telegram
+    conn.commit()
+    conn.close()
 
 def check_user(user_id):
     cursor.execute('SELECT 1 FROM test WHERE user_id = ? LIMIT 1', (user_id,))
